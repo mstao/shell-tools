cd369090649f17449e8d49dd724834c01ee81a21
mstao <499445428@qq.com>
2020-01-17 15:11:40 +0800

    Update Log

--- src/main/java/me/mingshan/tool/shell/log/LogType.java
+++ src/main/java/me/mingshan/tool/shell/log/LogType.java
@@ -1,13 +1,3 @@
-/*
- * 版权所有(C)，上海海鼎信息工程股份有限公司，2020，所有权利保留。
- *
- * 项目名：code
- * 文件名：LogType.java
- * 模块说明：
- * 修改历史：
- * 2020/1/17 下午2:18 - hanjuntao - 创建。
- *
- */
 
 package me.mingshan.tool.shell.log;
 
